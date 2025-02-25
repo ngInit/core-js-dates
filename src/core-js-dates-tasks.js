53dbcda5919a4f172ee91f52c99d9c24313ae5a3
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2025-02-25 22:51:20 +0100

    refactor: remove comment from getQuarter

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -243,10 +243,9 @@ function getNextFridayThe13th(/* date */) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  // const currentMonth = date.getMonth() + 1;
-  // return Math.ceil(currentMonth / 3);
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const currentMonth = date.getMonth() + 1;
+  return Math.ceil(currentMonth / 3);
 }
 
 /**
