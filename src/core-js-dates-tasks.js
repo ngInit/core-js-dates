cf6b672a013397e082e059201397c152f0ca7535
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2025-02-25 22:54:32 +0100

    refactor: remove comment from getCountDaysOnPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -99,12 +99,11 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  // const currentUTCDate = Date.parse(dateStart);
-  // const expireUTCDate = Date.parse(dateEnd);
-  // const secondsLeft = Math.floor((expireUTCDate - currentUTCDate) / 1000);
-  // return Math.floor(secondsLeft / (24 * 3600)) + 1;
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const currentUTCDate = Date.parse(dateStart);
+  const expireUTCDate = Date.parse(dateEnd);
+  const secondsLeft = Math.floor((expireUTCDate - currentUTCDate) / 1000);
+  return Math.floor(secondsLeft / (24 * 3600)) + 1;
 }
 
 /**
